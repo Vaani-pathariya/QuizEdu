3635f2d11bcf14614c017098d2e513c7130d6689
YogitaSingh9 <115174566+YogitaSingh9@users.noreply.github.com>
2023-03-08 00:49:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './components/Form';
-
 function App() {
   return (
-    <div className="page">
-      <Form></Form>
+    <div className="App">
+      hekki
     </div>
   );
 }
+
 export default App;
