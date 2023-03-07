68dcd3da9017a5577e2bbb68fb07524997ea9060
YogitaSingh9 <115174566+YogitaSingh9@users.noreply.github.com>
2023-03-08 01:34:22 +0530

    Add files via upload

---
+++ frontend/src/Form.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './login.css';
+const Form=()=> {
+    return(
+        <section>
+    <div className="cover">
+        <h1>Login</h1>
+        <p className="para">Welcome back! Please enter your details</p>
+        
+        <input type="text" placeholder="username"/> 
+       
+        <input type="password" placeholder="password"/>
+        <div className="login-btn">Login</div>
+        <p className="signup-line">Don't have an account? <span><a href ="signup">Signup</a></span></p>
+    </div>
+    <div className="cov"></div>
+    </section>
+    )  
+}
+export default Form
