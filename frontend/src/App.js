c8ba378a47dd5d77eb3e248a8d79ee2a0dbdd99d
YogitaSingh9 <115174566+YogitaSingh9@users.noreply.github.com>
2023-03-09 15:36:41 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="page">
+      <Form></Form>
+    </div>
+  );
+}
+export default App;
